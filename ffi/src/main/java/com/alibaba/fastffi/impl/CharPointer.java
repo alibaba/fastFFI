d8267ee270c67bc051782bc34ca08462be0dbaef
Tao He <sighingnow@gmail.com>
2022-02-17 11:34:48 +0800

    Add CharPointer type to the base package.
    Signed-off-by: Tao He <sighingnow@gmail.com>

---
+++ ffi/src/main/java/com/alibaba/fastffi/impl/CharPointer.java
@@ -0,0 +1,11 @@
+package com.alibaba.fastffi.impl;
+
+import com.alibaba.fastffi.FFIGen;
+import com.alibaba.fastffi.FFIPointer;
+import com.alibaba.fastffi.FFITypeAlias;
+
+@FFIGen
+@FFITypeAlias("char")
+public interface CharPointer extends FFIPointer
+{
+}
